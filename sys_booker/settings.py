427bcf2931746a0c58b869ea0f79dcd2b381773f
ASSONGBA Méchac <mechacassongba68@gmail.com>
2025-08-13 16:25:26 +0200

    Installation de swagger drf_yasg

--- sys_booker/settings.py
+++ sys_booker/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u@5o7qp#c_)-4nosg)%n)h)lbrc94%laeddb)_7-xw)-l56_cs'
+SECRET_KEY = (
+    'django-insecure-u@5o7qp#c_)-4nosg)%n)h)lbrc94%laeddb)_7-xw)-'
+    'l56_cs'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -41,6 +44,7 @@ INSTALLED_APPS = [
     
     'booker',
     'rest_framework',
+	'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -89,16 +93,28 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'MinimumLengthValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'CommonPasswordValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'NumericPasswordValidator'
+        ),
     },
 ]
 
--- sys_booker/urls.py
+++ sys_booker/urls.py
@@ -16,10 +16,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Sys Booker API",
+        default_version="v1",
+        description="Documentation pour l'API de Sys Booker",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/', include('booker.urls'))
+    path('api/', include([
+        path('', include('booker.urls')),
+        path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
+    ])),
 ]
+
