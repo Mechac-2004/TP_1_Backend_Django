2eb25dc5e60d10bbfe72a79ca359b31381cbe564
ASSONGBA Méchac <mechacassongba68@gmail.com>
2025-08-26 17:22:02 +0200

    Connection de l'api à Postgres

--- core/settings.py
+++ core/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = '9ecvj%em^o-badfay8z3uil1#@281vj&5*8ea*lih((35t67ys'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=True, cast=bool)
+DEBUG = True  
 
 ALLOWED_HOSTS = []
 
@@ -79,20 +79,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': config("DB_NAME"),
-    #     'USER': config("DB_USER"),
-    #     'PASSWORD': config("DB_PASSWORD"),
-    #     'HOST': config("DB_HOST", default="localhost"),
-    #     'PORT': config("DB_PORT", default="5432"),
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),     
+        'USER': config('DB_USER'),         
+        'PASSWORD': config('DB_PASSWORD'), 
+        'HOST': config('DB_HOST'),       
+        'PORT': '5432',            
+    }
 }
+
 
 
 # Password validation
