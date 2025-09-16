b0f2ddc47e7f04fa050c256a84c24adb7042e83b
blacky37 <ihouenou90@gmail.com>
2025-09-16 10:36:44 +0100

    Event booking api with documentation

--- booking/permissions.py
+++ booking/permissions.py
@@ -21,4 +21,5 @@ class IsAdminUserOnly(BasePermission):
     peut accéder à la vue.
     """
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated and (request.user.is_staff or request.user.role == "admin"))
+        return bool(request.user and request.user.is_authenticated and (request.user.is_superuser
+                or request.user.groups.filter(name__in=['Admin']).exists()))
